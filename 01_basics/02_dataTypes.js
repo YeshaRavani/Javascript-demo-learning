c6c66c5853f782a03113f53362724e5c7d2bdbd2
YeshaRavani <yesharavani20@gmail.com>
2025-07-08 04:57:52 +0000

    changes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,6 @@
 "use strict";  //treat all JS code as newer version
+
+// alert(3+3) //this syntax works for browser for popups, but not for nodejs
+
+console.log(3+3);
+// to get documentation use mdn
