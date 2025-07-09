94179c1b53ef6ab371ab6ba8fcaeed60faf9d56e
YeshaRavani <yesharavani20@gmail.com>
2025-07-09 03:08:25 +0000

    changed

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -24,9 +24,42 @@ console.log(typeof null);  //object
 
 // 2 main types : primitive and non-primitive : depends on how they are accesses and stored
 
-// primitive: 
+// primitive: (7 types) --> call by value
+
+//String
+//Number
+//Boolean
+//Null
+//undefined
+//symbol (to make something unique) typeof --> symbol
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId); //false
+
+const bigNum=232324223423234232232n
+
+//BigInt --> for extremely big numbers  ; typeof --> bigInt
+
+// Reference (Non primitive) --> direct memmory access
+
+//Arary // typeof --> object
+//Objects // typeof --> object
+//Functions // typeof --> function object
+
+const heros=['shaktiman', 'naagraj', 'doga'];
+let myObj = {
+    name: 'yesha',
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNum);
+
+// Is javascript is dynamically typed/ statically types : dynamically typed language
 
 
-
-
-
+ 
